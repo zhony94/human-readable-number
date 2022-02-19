6a8f4eec6532174c845c019358d5cea72adf1f45
zhony94 <64091893+zhony94@users.noreply.github.com>
2022-02-19 10:29:29 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 module.exports = function toReadable (number) {
-  
+  module.exports = function toReadable (number) {  
+    base = {
+        "0": "zero",
+        "1": "one",
+        "2": "two",
+        "3": "three",
+        "4": "four",
+        "5": "five",
+        "6": "six",
+        "7": "seven",
+        "8": "eight",
+        "9": "nine",
+        "10": "ten",
+        "11": "eleven",
+        "12": "twelve",
+        "13": "thirteen",
+        "14": "fourteen",
+        "15": "fifteen",
+        "16": "sixteen",
+        "17": "seventeen",
+        "18": "eighteen",
+        "19": "nineteen",
+        "20": "twenty",
+        "30": "thirty",
+        "40": "forty",
+        "50": "fifty",
+        "60": "sixty",
+        "70": "seventy",
+        "80": "eighty",
+        "90": "ninety"
+    };
+    
+    if(number>=0 && number<100){
+        return belowHundred(number)
+    }else if(number>=100 && number<1000){
+        return aboveHundred(number)
+    }
+}
+    
+    
+function belowHundred(number){
+    if(!(base[number] === undefined)){
+        return base[number]
+    }else {
+        const firstPart = Math.floor(number/10)*10;
+        const secondPart = number%10
+        return `${base[firstPart]} ${base[secondPart]}`
+    }
+}
+
+function aboveHundred(number){
+    const firstPart = Math.floor(number/100);
+    const secondPart = belowHundred(number - firstPart*100);
+    if(number%100 ===0){
+        return `${base[firstPart]} hundred`
+    }else {            
+        return `${base[firstPart]} hundred ${secondPart}`
+    }
+    
+}
 }
